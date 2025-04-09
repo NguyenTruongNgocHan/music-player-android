5446153e04f7f4e8de28017d46778ad708241377
Hân Nguyễn Trương Ngọc <23520434@ms.uit.edu.vn>
2025-04-09 19:24:20 +0700

    init

---
+++ app/src/main/java/com/example/frontend/LoadingActivity.kt
@@ -0,0 +1,24 @@
+package com.example.frontend
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.frontend.LoginActivity
+
+import com.example.frontend.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading) // Đảm bảo đúng tên file XML
+
+        // Chờ 3 giây rồi chuyển sang màn hình Login
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Đóng LoadingActivity để không quay lại được
+        }, 3000) // Thời gian delay (3 giây)
+    }
+}
